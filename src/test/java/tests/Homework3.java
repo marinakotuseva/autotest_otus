9a79f18d67469507397de127dd352cd9bb7dd3e7
Dmitriy Erokhin <nefariusmag@gmail.com>
2018-04-28 04:14:14 +0300

    Поправил README.md

--- src/test/java/tests/Homework3.java
+++ src/test/java/tests/Homework3.java
@@ -1,17 +1,37 @@
 package tests;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import utils.WebDriverManager;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Homework3 {
 
-//    public void getSite(String text) {
-//        WebDriverManager.getInstance().get("http://blazedemo.com/");
-//    }
+    private String URL_what_you_need= "http://blazedemo.com/";
 
     @Test
-    public  void getSite() {
-        WebDriverManager.getInstance().get("http://blazedemo.com/");
+    public void getSite() {
+
+        WebDriverManager.getInstance().get(URL_what_you_need);
+
+        WebElement findbutton = WebDriverManager.getInstance().findElement(By.cssSelector("input[type='submit']"));
+
+        findbutton.click();
+
+        WebElement findnewopen = WebDriverManager.getInstance().findElement(By.className("table"));
+
+        assertTrue(findnewopen.isDisplayed());
+
+    }
+
+    @AfterClass
+    public void aftertest() {
+
+        WebDriverManager.getInstance().quit();
+
     }
 }
